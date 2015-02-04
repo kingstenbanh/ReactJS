e4828236f2d12094b016eacfc4fc7a0f4f969f60
Kingsten Banh <kingsten.banh@gmail.com>
2015-02-03 23:03:15 -0700

    complete tutorial

--- react-comment/server.js
+++ react-comment/server.js
@@ -6,7 +6,7 @@ var app = express();
 
 var comments = JSON.parse(fs.readFileSync('_comments.json'));
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
